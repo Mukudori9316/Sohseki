dae9d22b9a51bdb5bc09e0fef732b8c722e062c1
Mukudori9316 <ryotaf1116@gmail.com>
2017-01-11 17:19:30 +0900

    3.1

--- reader.py
+++ reader.py
@@ -29,3 +29,32 @@ f.close()
 f = open('wlist.txt', 'w', encoding='utf-8')
 f.write(words)
 f.close()
+
+txt = ''
+words = ''
+with open('wagahaiwa_nekodearu.txt', 'r', encoding='utf-8') as f:
+    wagahaiwa_nekodearu = list(f)
+
+wagahaiwa_nekodearu = wagahaiwa_nekodearu[22:-14]
+
+for n, i in enumerate(wagahaiwa_nekodearu):
+    if i != '\n':
+        txt += i[:-1]
+t = Tokenizer()
+tokens = t.tokenize(txt)
+
+
+for token in tokens:
+    n1 = token.surface
+    n2 = token.part_of_speech.split(',')[0]
+    n3 = token.part_of_speech.split(',')[1]
+    n4 = token.part_of_speech.split(',')[2]
+    n5 = token.infl_form
+    n6 = token.base_form
+    if n2 != u'記号':
+        words += (n1 + ',' + n2 + ',' + n3 + ',' +
+                  n4 + ',' + n5 + ',' + n6 + '\n')
+f.close()
+f = open('wlist.txt', 'a', encoding='utf-8')
+f.write(words)
+f.close()
